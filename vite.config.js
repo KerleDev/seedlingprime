3dcfbbc47793d0db26af7d75db5ac425de9aba0a
KL <154016556+KerleDev@users.noreply.github.com>
2025-09-24 22:13:19 +0300

    no limit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'stats.html',   // הקובץ עם הגרף יווצר בתוך dist/
+      template: 'treemap',      // יש גם sunburst/other אם תרצי
+      open: true,               // יפתח אוטומטית בדפדפן אחרי build
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 2000, // מעלה את המגבלה כדי להעלים את האזהרה
+  },
 })

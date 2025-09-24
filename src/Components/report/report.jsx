4f265b8338f9c9c89e20f5f159cff4b5123142c9
LeonEidelman <leoneidelman09@gmail.com>
2025-09-24 11:18:19 +0300

    first

--- src/Components/report/report.jsx
+++ src/Components/report/report.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
 export default function Report() {
-  return <div>hello from Report</div>;
+  return (
+    <div>
+      <section style={{ display: 'flex' }}>
+        <div>
+          <div>Seedling </div>
+          <p>
+            making you one big step closer to your financial freedom
+            by bringing you the most accurate stock findings
+          </p>
+        </div>
+        <div>logo</div>
+      </section>
+    </div>
+  );
 }

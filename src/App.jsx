c34605d25fb918863c7156f93f008bae7c2c8dba
LeonEidelman <leoneidelman09@gmail.com>
2025-09-24 11:08:02 +0300

    hello from Report

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./Layout";
-import Dashboard from "./Components/Dashboard/Dashboard";
-import Landing from "./Components/Landing/Landing";
-import "./App.css";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './Layout';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Landing from './Components/Landing/Landing';
+import './App.css';
+import Report from './Components/report/Report';
 
 function App() {
   return (
     <Routes>
       {/* All pages share the same Layout (TopBar + Footer) */}
-      <Route path="/" element={<Layout />}>
+      <Route
+        path="/"
+        element={<Layout />}
+      >
         {/* Default page → Landing */}
-        <Route index element={<Landing />} />
+        <Route
+          index
+          element={<Landing />}
+        />
 
         {/* Dashboard page */}
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route
+          path="dashboard"
+          element={<Dashboard />}
+        />
+        <Route
+          path="report"
+          element={<Report />}
+        />
       </Route>
 
       {/* Fallback: any unknown route redirects to "/" (Landing) */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route
+        path="*"
+        element={
+          <Navigate
+            to="/"
+            replace
+          />
+        }
+      />
     </Routes>
   );
 }
